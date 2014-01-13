f120ee007024dfa6a3ef607422501d6824bb1603
Trent Oswald <trentoswald@therebelrobot.com>
2014-01-13 15:32:48 -0700

    added additional state identifiers

--- source/js/app.js
+++ source/js/app.js
@@ -32,7 +32,7 @@ App.run(["$rootScope", '$window', "$location", function($rootScope, $window, $lo
 		verbose : false
   });
   function setDevice(){
-  	console.log('resize');
+  	console.log('resize (6683160)');
     $rootScope.ifDesktop = (/windows|linux|os\s+[x9]|solaris|bsd/i).test(navigator.userAgent) && !(/ipod|iphone/i).test(navigator.userAgent);
     $rootScope.ifTablet = (/ipad/i).test(navigator.userAgent)||((/android/i).test(navigator.userAgent) && !(/mobile/i).test(navigator.userAgent));
     $rootScope.ifMobile = (/ipod|iphone/i).test(navigator.userAgent)|| ((/android/i).test(navigator.userAgent) && (/mobile/i).test(navigator.userAgent)) ||(!$rootScope.ifDesktop && !$rootScope.ifTablet);
@@ -44,4 +44,35 @@ App.run(["$rootScope", '$window', "$location", function($rootScope, $window, $lo
     $rootScope.menuExpanded = ($rootScope.ifMobile || $rootScope.ifTablet) ? false : true;
   }
   setDevice();
+  $rootScope.position = ['pos0','pos1','pos2','pos3','pos4','pos5'];
+  $rootScope.setPosition = function(state){
+    switch (state){
+        case 'main':
+          $rootScope.position = ['pos0','pos1','pos2','pos3','pos4','pos5'];
+          break;
+
+        case 'stack':
+          $rootScope.position = ['pos-1','pos0','pos1','pos2','pos3','pos4'];
+          break;
+          
+        case 'projects':
+          $rootScope.position = ['pos-2','pos-1','pos0','pos1','pos2','pos3'];
+          break;
+          
+        case 'about':
+          $rootScope.position = ['pos-3','pos-2','pos-1','pos0','pos1','pos2'];
+          break;
+          
+        case 'contact':
+          $rootScope.position = ['pos-4','pos-3','pos-2','pos-1','pos0','pos1'];
+          break;
+          
+        case '404':
+          $rootScope.position = ['pos-5','pos-4','pos-3','pos-2','pos-1','pos0'];
+          break;
+          
+
+    }
+
+  };
 }]);
